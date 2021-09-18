ce1a6b80226d724f75162e4a688dbee297010e0b
Yusepp <xxhunterspainxx@gmail.com>
2021-09-19 00:35:46 +0200

    v0.1 official release

--- music_bot.py
+++ music_bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import asyncio
 from discord.ext.commands.core import after_invoke
 from discord.ext.commands import bot
 from discord.ext import commands
@@ -18,7 +19,7 @@ class MusicBot(commands.Cog):
 
         self.voice_channel = None
         self.current_song = None
-    
+        self.tmp = False
 
     def search_youtube(self, item) -> dict:
 
@@ -34,13 +35,13 @@ class MusicBot(commands.Cog):
                  'title': info['title'] }
 
 
-    async def next(self,ctx) -> None:
+    def next(self,ctx) -> None:
 
         if self.q:
             self.is_playing = True
             url = self.q[0][0]['source']
             self.current_song = self.q.pop(0)
-            await ctx.send("REPRODUCIENDO LA SIGUIENTE CANCION QUE FLIPAS: {} \n".format(self.current_song[0]['title']))
+            asyncio.run_coroutine_threadsafe(ctx.send("REPRODUCIENDO LA SIGUIENTE CANCION QUE FLIPAS: **{}** \n".format(self.current_song[0]['title'])),self.bot.loop)
             self.voice_channel.play(discord.FFmpegPCMAudio(url, **self.FFMEPG_OPTIONS), after=lambda e: self.next(ctx))
 
         else:
@@ -55,6 +56,7 @@ class MusicBot(commands.Cog):
 
             if not self.voice_channel or not self.voice_channel.is_connected():
                 self.voice_channel = await self.q[0][1].connect()
+                await ctx.send("LA BOMBA BATISTA YA ESTA AQUI!")
             
             self.current_song = self.q.pop(0)
             self.voice_channel.play(discord.FFmpegPCMAudio(url, **self.FFMEPG_OPTIONS), after=lambda e: self.next(ctx))
@@ -77,12 +79,23 @@ class MusicBot(commands.Cog):
                 await ctx.send("TE HAS EQUIVOCADO EN ALGO O LOS SERVIDORES VAN MAL NOSE YO SOY SOLO BATISTA XD\n")
             
             else:
-                await ctx.send("HERMANO TE REGALAS HEMOS ENCONTRADO LA CANCIÓN Y LA HEMOS METIDO EN LA COLA\n")
-                await ctx.send("SE HA AÑADIDO: "+song['title'].upper()+" A LA LISTA\n")
+                #await ctx.send("HERMANO TE REGALAS HEMOS ENCONTRADO LA CANCIÓN Y LA HEMOS METIDO EN LA COLA\n")
+                await ctx.send("SE HA AÑADIDO: **"+song['title'].upper()+"** A LA LISTA\n")
                 self.q.append([song, vc])
                 if not self.is_playing:
                     self.is_playing = True
                     await self.play(ctx)
+    
+    @commands.command()
+    async def pall(self, ctx, *args) -> None:
+        query = " ".join(args).split("|")
+        query = [song[1:] for song in query]
+
+        for song in query:
+            args = song.split(" ")
+            await self.p(ctx,args)
+
+
 
 
     @commands.command()
@@ -91,9 +104,21 @@ class MusicBot(commands.Cog):
             await ctx.send("NO HAY CANCIONES MANIN AÑADELAS")
 
         else:
-            string = [("{}- ".format(i+1)+str(song[0]['title'])).upper() for i,song in enumerate([self.current_song]+self.q)]
+            string = [("{} - ".format(i+1)+"**"+str(song[0]['title'])).upper()+"**" for i,song in enumerate([self.current_song]+self.q)]
+            string[0]+="(current)"
             string = "\n".join(string)
-            await ctx.send("LISTA ACTUAL: \n"+string)
+            await ctx.send("LISTA ACTUAL: \n ------------------------------------------------------------- \n"+string)
+    
+    @commands.command()
+    async def queue(self, ctx) -> None:
+        if not self.q and not self.is_playing:
+            await ctx.send("NO HAY CANCIONES MANIN AÑADELAS")
+
+        else:
+            string = [("{} - ".format(i+1)+"**"+str(song[0]['title'])).upper()+"**" for i,song in enumerate([self.current_song]+self.q)]
+            string[0]+="(current)"
+            string = "\n".join(string)
+            await ctx.send("LISTA ACTUAL: \n ------------------------------------------------------------- \n"+string)
     
 
     @commands.command()
@@ -103,9 +128,16 @@ class MusicBot(commands.Cog):
             await self.next(ctx)
 
         else:
-            await ctx.send("COMO VAS A SKIPPEAR ALGO QUE NO EXISTE CRACK")
             self.voice_channel.stop()
     
+    @commands.command()
+    async def skip(self, ctx) -> None:
+        if self.q:
+            self.voice_channel.stop()
+            await self.next(ctx)
+
+        else:
+            self.voice_channel.stop()
 
     @commands.command()
     async def stop(self, ctx) -> None:
@@ -116,10 +148,24 @@ class MusicBot(commands.Cog):
     @commands.command()
     async def rm(self, ctx, i) -> None:
         try:
-            song = ([self.curren_song]+self.q)[i-1]
-            await ctx.send("SE HA ELIMINADO: "+song['title'].upper()+" DE LA LISTA\n")
-            self.q.remove(song)
-        except:
+            i = int(i)
+            song = [self.current_song]+self.q
+            song = song[i-1]
+            await ctx.send("SE HA ELIMINADO: **"+song[0]['title'].upper()+"** DE LA LISTA\n")
+            self.q.pop(i-2)
+        except Exception as e:
+            await ctx.send("ESO NO ES UNA POSICIÓN DE LA LISTA CHULO\n")
+    
+
+    @commands.command()
+    async def remove(self, ctx, i) -> None:
+        try:
+            i = int(i)
+            song = [self.current_song]+self.q
+            song = song[i-1]
+            await ctx.send("SE HA ELIMINADO: **"+song[0]['title'].upper()+"** DE LA LISTA\n")
+            self.q.pop(i-2)
+        except Exception as e:
             await ctx.send("ESO NO ES UNA POSICIÓN DE LA LISTA CHULO\n")
 
         
@@ -129,6 +175,28 @@ class MusicBot(commands.Cog):
         await self.voice_channel.disconnect()
         self.is_playing = False
         self.q = []
+    
+    @commands.command()
+    async def disconnect(self,ctx) -> None:
+        await self.voice_channel.disconnect()
+        self.is_playing = False
+        self.q = []
+    
+    
+    @commands.command()
+    async def h(self, ctx) -> None:
+        commands = ["-h -help", "-p <name or url>", "-q -queue", "-s -skip", "-stop", "-rm -remove <index>", "-dc -disconnect"]
+        info = ["AQUI TIENES TODA LA INFO DE LOS COMANDOS FIERA", "AÑADE CANCIONES PARA REPRODUCIR MAQUINA", "MUESTRA LAS CANCIONES EN LA COLA DE REPRODUCCIÓ DIPLODOCUS",
+                "CUANDO TE DA PALO Y QUIERES LA SIGUIENTE WEON", "PARAR DE REPRODUCIR MUSICA JEFE", "ELIMINAR UNA DE LA LISTA PORQUE TE HAS EQUIVOCADO MERLUZO", "ME DESPIDES DEL TRABAJO TE MATO"]
+        
+        string = ["{} : **{}.**".format(commands[i],info[i]) for i in range(len(commands))]
+        string[0] = "LISTA DE COMANDOS \n ------------------------------------------------------------ \n" + string[0] 
+        string = "\n".join(string)
+
+        await ctx.send(string)
+
+
+
     
 
 def run() -> None:
